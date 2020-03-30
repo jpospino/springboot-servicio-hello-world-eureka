44dadb84521b26f29914d8d09d08706093cfea3d
Juan Pablo Ospino Solano <jp.ospino@uniandes.edu.co>
2020-03-30 16:43:42 -0500

    se añade servicio zuul

--- src/main/java/com/jospino/springboot/app/hello/world/heureka/SpringbootServicioHelloWorldEurekaApplication.java
+++ src/main/java/com/jospino/springboot/app/hello/world/heureka/SpringbootServicioHelloWorldEurekaApplication.java
@@ -3,7 +3,10 @@ package com.jospino.springboot.app.hello.world.heureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class SpringbootServicioHelloWorldEurekaApplication {
